6eb886761c6f4028e014a3f89a5e4ad93120ca3c
Meas Sotheareach <sotheach1000@gmail.com>
2025-11-25 19:58:05 +0700

    update deploy

--- database/migrations/2023_11_29_035432_create_chats_table.php
+++ database/migrations/2023_11_29_035432_create_chats_table.php
@@ -4,21 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('chats', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('sender_id');
-            $table->foreignId('receiver_id');
-            $table->text('message');
-            $table->boolean('seen')->default(0);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('chats')) {
+            Schema::create('chats', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('sender_id');
+                $table->foreignId('receiver_id');
+                $table->text('message');
+                $table->boolean('seen')->default(0);
+                $table->timestamps();
+            });
+        } else {
+            Schema::create('chats', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('sender_id');
+                $table->foreignId('receiver_id');
+                $table->text('message');
+                $table->boolean('seen')->default(0);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
